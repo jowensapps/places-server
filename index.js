f24d26d770e98afb0f81c6d78a416a3d374581d5
jowensapps <j.owens.apps@gmail.com>
2026-04-11 16:00:27 -0400

    Update index.js with debug-sync

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ import "dotenv/config";
 
 import { getNearbyPlaces } from "./places.js";
 import { getDirectionsDistance } from "./directions.js";
-import "./redis.js"; 
+import "./redis.js";
+import debugSync from "./debug-sync.js";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(debugSync);
 
 /**
  * Health check (used by UptimeRobot)
